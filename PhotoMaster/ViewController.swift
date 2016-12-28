9350353d13089c66f30fcedd361515a2da5ff6a7
Life is Tech <lifeistech@x-t47.local>
2016-12-28 09:46:36 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Life is Tech on 2016/12/28.
+//  Copyright © 2016年 Yuki Sekido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
